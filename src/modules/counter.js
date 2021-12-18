90184dc588c25e0f95a23c49c097be2255f2cdb6
seilylook <seilylook@naver.com>
2021-12-18 15:33:24 +0900

    feat: redux hook을 이용해라

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,4 +1,4 @@
-import { createAction, handleAction } from 'redux-actions';
+import { createAction, handleActions } from 'redux-actions';
 
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
@@ -10,7 +10,7 @@ const initialState = {
   number: 0,
 };
 
-const counter = handleAction(
+const counter = handleActions(
   {
     [INCREASE]: (state, action) => ({ number: state.number + 1 }),
     [DECREASE]: (state, action) => ({ number: state.number - 1 }),
